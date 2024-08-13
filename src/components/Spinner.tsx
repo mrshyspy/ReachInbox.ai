e8c8f31e22e1ea4cb822ecace51b76d76d49ce73
Sohil khan <92449631+mrshyspy@users.noreply.github.com>
2024-08-13 11:37:51 +0530

    Update Spinner.tsx

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,8 +3,7 @@ function Spinner() {
   return (
     <div className="flex justify-center items-center h-full">
       <div
-        className="w-16 h-16 border-8 border-blue-600 dark:border-blue-400 border-t-transparent border-solid rounded-full animate-spin"
-        style={{ borderWidth: '4px' }} // Optional: Override border width if needed
+        className="w-16 h-16 border-8 border-blue-600 dark:border-red-600 border-t-transparent border-solid rounded-full animate-spin"
       ></div>
     </div>
   );
